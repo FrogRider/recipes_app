0ad1dcebd15f35da10a6a9c1a1bf1f447e5b2df7
Jekos <jhonny.lbk@gmail.com>
2019-10-29 02:21:41 +0200

    Updates

---
+++ precache-manifest.4284b499e08ccd9a7e382d798838dfe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7539c6df2fd1c4157ba7843600c6a23",
+    "url": "/recipes_app/index.html"
+  },
+  {
+    "revision": "6d3aa923728524a380ad",
+    "url": "/recipes_app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "9a78fd29baf87ca4f611",
+    "url": "/recipes_app/static/js/2.229946a6.chunk.js"
+  },
+  {
+    "revision": "6d3aa923728524a380ad",
+    "url": "/recipes_app/static/js/main.b51a5b3a.chunk.js"
+  },
+  {
+    "revision": "c27e1f4bf44296f2825a",
+    "url": "/recipes_app/static/js/runtime~main.b088218d.js"
+  }
+]);
